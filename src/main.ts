31a4d163c8c6aeae9ee25fba9b5b06a4d1c16cb7
Minh Nguyen <anhminh1782002@gmail.com>
2024-12-26 15:44:34 +0700

    feat: config to deploy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://next-admin-floral-jevrmfprw-minhng178s-projects.vercel.app/',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const config = new DocumentBuilder()
     .setTitle('Floral Swagger')
